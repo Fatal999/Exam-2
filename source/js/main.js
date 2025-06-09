cb8fc7874fb6674f35c7e9bde6705df2aa67c0da
Fatal999 <euronymous.ts@yandex.ru>
2025-06-09 15:46:26 +0300

    test

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,12 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
 
 import './swiper-bundle.js';
 
-import './blocks/hero.js';
-import './blocks/price.js';
-import './blocks/video.js';
-import './blocks/juri.js';
-import './blocks/faq.js';
-import './blocks/form.js';
+import './hero.js';
+import './price.js';
+import './video.js';
+import './juri.js';
+import './faq.js';
+import './form.js';
